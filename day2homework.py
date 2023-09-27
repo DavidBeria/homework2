6136d0e7e53852e362e20e1d3299a5b4ee8e1047
DavidBeria <143294896+DavidBeria@users.noreply.github.com>
2023-09-27 22:41:22 +0400

    Add files via upload

---
+++ day2homework.py
@@ -0,0 +1,17 @@
+mom = 41
+mom = mom + 5
+dad = 45
+dad = dad + 5
+brother = 10
+brother = brother + 5
+brother2 = 8
+brother2 = brother2 + 5
+me = 12
+me = me + 5
+
+
+print("mom ", mom)
+print("dad ", dad)
+print("brother ", brother)
+print("brother2 ", brother2)
+print("me ", me)
